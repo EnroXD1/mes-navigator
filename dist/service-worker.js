8b223113c5c15c957dcf2e72f359613fec3e6356
EnroXD1 <49277608+EnroXD1@users.noreply.github.com>
2026-09-11 00:26:11 +0300

    Release version 0.1.0

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "ceh-znaniy-v1";
+const CACHE = "ceh-znaniy-0.1.0";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
